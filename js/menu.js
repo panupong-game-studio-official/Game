002761f6f8437141193231e5d5f21ea15d1c1514
panupong-game-studio-official <panupongartpairin914m@gmail.com>
2026-01-23 21:46:02 +0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,7 @@
-
+export function showMenu() {
+  const menu = document.getElementById("menu");
+  menu.innerHTML = `
+    <h1>🐒 Monkey Garden Run 🍌</h1>
+    <button onclick="startGame()">▶️ เริ่มเกม</button>
+  `;
+}
