35a8055b0ad1d456b1f3c53af3af9c635fa28276
panupong-game-studio-official <panupongartpairin914m@gmail.com>
2026-01-23 21:45:39 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+import { showMenu } from './menu.js';
+import { initGame } from './game.js';
 
+showMenu();
+
+window.startGame = () => {
+  document.getElementById("menu").style.display = "none";
+  initGame();
+};
