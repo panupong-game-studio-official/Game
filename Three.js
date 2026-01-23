53f20f8ca838396cf5b4969fae68b5ab9727d8c8
panupong-game-studio-official <panupongartpairin914m@gmail.com>
2026-01-23 22:15:59 +0700

    Update Three.js

--- Three.js
+++ Three.js
@@ -1,1 +1,7 @@
+// สร้างตัวละครแบบง่าย
+const geometry = new THREE.BoxGeometry(1, 2, 1);
+const material = new THREE.MeshStandardMaterial({ color: 0xffaa00 });
+const player = new THREE.Mesh(geometry, material);
 
+player.position.y = 1;
+scene.add(player);
