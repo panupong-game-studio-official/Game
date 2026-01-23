3ef530de6e384744e304edf23fb8848bc07fab3c
panupong-game-studio-official <panupongartpairin914m@gmail.com>
2026-01-23 21:46:22 +0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,9 @@
+import { createPlayer } from './player.js';
+import { createWorld } from './world.js';
 
+export function initGame() {
+  console.log("Game Started");
+
+  createWorld();
+  createPlayer();
+}
