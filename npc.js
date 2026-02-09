cdda559279834aa34099d043ea79656a0b0bc2d9
panupong-game-studio-official <panupongartpairin914m@gmail.com>
2026-02-09 16:07:27 +0700

    Update npc.js

--- npc.js
+++ npc.js
@@ -1,1 +1,31 @@
+class NPC {
+  constructor(name, gender, x, y) {
+    this.name = name;
+    this.gender = gender;
+    this.x = x;
+    this.y = y;
 
+    this.energy = 100;
+    this.hunger = 0;
+    this.state = "idle";
+  }
+
+  update() {
+    this.hunger += 0.02;
+    this.energy -= 0.01;
+
+    AI.think(this);
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.gender === "male" ? "cyan" : "pink";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 8, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+}
