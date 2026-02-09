11470eef5c5f629a75282db17e1b2f4309f21073
panupong-game-studio-official <panupongartpairin914m@gmail.com>
2026-02-09 16:07:00 +0700

    Update world.js

--- world.js
+++ world.js
@@ -1,1 +1,19 @@
+const World = {
+  width: 800,
+  height: 500,
+  time: 0,
+  day: 1,
 
+  update() {
+    this.time++;
+    if (this.time % 600 === 0) {
+      this.day++;
+      log(`🌞 Day ${this.day} begins`);
+    }
+  },
+
+  draw(ctx) {
+    ctx.fillStyle = "#2c5f2d"; // ground
+    ctx.fillRect(0, 0, this.width, this.height);
+  }
+};
