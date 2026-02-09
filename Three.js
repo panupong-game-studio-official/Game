3d7232c2c0a0f5bb5e9e1f7df0bdaf6f1634bdd5
panupong-game-studio-official <panupongartpairin914m@gmail.com>
2026-02-09 15:47:10 +0700

    Delete Three.js

--- Three.js
+++
@@ -1,7 +0,0 @@
-// สร้างตัวละครแบบง่าย
-const geometry = new THREE.BoxGeometry(1, 2, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0xffaa00 });
-const player = new THREE.Mesh(geometry, material);
-
-player.position.y = 1;
-scene.add(player);
